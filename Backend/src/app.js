f4191bb120b27217d06483217d780872ee7a86b7
Shivam mishra <114843423+Shiv259@users.noreply.github.com>
2025-07-08 16:37:16 +0530

    added cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const airoutes = require('./routes/ai.routes')
 
+const cors = require('cors')
+
 
 const app = express();
+
+app.use(cors())
 
 app.use(express.json())
 app.get('/', (req,res) => {
